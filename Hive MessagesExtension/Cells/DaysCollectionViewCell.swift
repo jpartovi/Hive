3b7167d9ecc1a117783f70997db2310ed1d184ee
Jude Partovi <jude@partovi.org>
2022-06-29 14:03:31 -0700

    Better Time Date Selector

--- Hive MessagesExtension/Cells/DaysCollectionViewCell.swift
+++ Hive MessagesExtension/Cells/DaysCollectionViewCell.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class DaysCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var dayOfWeekLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
+    //@IBOutlet weak var dayOfWeekLabel: UILabel!
+    //@IBOutlet weak var dateLabel: UILabel!
     
     var day: Day? {
         didSet {
@@ -27,14 +27,52 @@ class DaysCollectionViewCell: UICollectionViewCell {
         }
     }
     
-    override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
-        super.apply(layoutAttributes)
+    var dayOfWeekLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        label.textColor = Style.lightColor
+        return label
+    }()
+    
+    var dateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        label.textColor = Style.lightColor
+        return label
+    }()
+     
+    static let reuseIdentifier = String(describing: DaysCollectionViewCell.self)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         
-        // dayOfWeekLabel.text = day!.formatDayOfWeek()
-        // dateLabel.text = day!.formatDate()
+        contentView.addSubview(dayOfWeekLabel)
+        contentView.addSubview(dateLabel)
     }
 
-    static let reuseIdentifier = String(describing: TimesCollectionViewCell.self)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        // This allows for rotations and trait collection
+        // changes (e.g. entering split view on iPad) to update constraints correctly.
+        // Removing old constraints allows for new ones to be created
+        // regardless of the values of the old ones
+        NSLayoutConstraint.activate([
+            dayOfWeekLabel.topAnchor.constraint(equalTo: topAnchor),
+            dayOfWeekLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            dateLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            dateLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+    }
 }
 
 // MARK: - Appearance
@@ -61,6 +99,7 @@ private extension DaysCollectionViewCell {
     
     func style() {
         
+        self.backgroundColor = Style.secondaryColor
         self.layer.cornerRadius = 20
     }
 }
--- Hive MessagesExtension/Cells/TimesCollectionViewCell.swift
+++ Hive MessagesExtension/Cells/TimesCollectionViewCell.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class TimesCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var timeOfDayLabel: UILabel!
-    @IBOutlet weak var startTimeLabel: UILabel!
-    @IBOutlet weak var endTimeLabel: UILabel!
+    //@IBOutlet weak var timeOfDayLabel: UILabel!
+    //@IBOutlet weak var startTimeLabel: UILabel!
+    //@IBOutlet weak var endTimeLabel: UILabel!
+    
+    
 
     var dayTimePair: DayTimePair? {
         didSet {
@@ -30,7 +32,63 @@ class TimesCollectionViewCell: UICollectionViewCell {
         }
     }
     
+    var timeOfDayLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        label.textColor = Style.lightColor
+        return label
+    }()
     
+    var startTimeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        label.textColor = Style.lightColor
+        return label
+    }()
+    
+    var endTimeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        label.textColor = Style.lightColor
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(timeOfDayLabel)
+        contentView.addSubview(startTimeLabel)
+        contentView.addSubview(endTimeLabel)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        // This allows for rotations and trait collection
+        // changes (e.g. entering split view on iPad) to update constraints correctly.
+        // Removing old constraints allows for new ones to be created
+        // regardless of the values of the old ones
+        NSLayoutConstraint.activate([
+            timeOfDayLabel.topAnchor.constraint(equalTo: topAnchor),
+            timeOfDayLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            startTimeLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            startTimeLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            endTimeLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            endTimeLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+    }
 
     static let reuseIdentifier = String(describing: TimesCollectionViewCell.self)
 }
